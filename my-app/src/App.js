77791b760dafc07b73966ea02a80d7d11cfaf3b1
Pre-11 <prernakumari7427@gmail.com>
2023-08-17 23:02:46 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import RegistrationForm from './Component/RegistrationForm';
+
+function App() {
+  return (
+    <div className="App">
+      <RegistrationForm />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Component/RegistrationForm.jsx
@@ -0,0 +1,15 @@
+
+
+import React from 'react'
+
+function RegistrationForm() {
+  return (
+    <div>
+        <h1>
+            Hello React World
+        </h1>
+    </div>
+  )
+}
+
+export default RegistrationForm
